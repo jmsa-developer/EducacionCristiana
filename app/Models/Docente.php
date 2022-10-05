ee1a4b0016368a3dbe03dad99d3350711fb7e172
Carlyn <carlynyepez12@gmail.com>
2022-10-04 22:29:31 -0400

    prueba

---
+++ app/Models/Docente.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use App\BaseModel;
+
+class Docente extends BaseModel
+{
+    public function __construct()
+    {
+        parent::__construct('docente');
+    }
+
+
+
+}
