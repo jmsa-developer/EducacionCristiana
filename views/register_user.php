3ba542533f2b7a1f828172404dd94b20e1da0ea0
Carlyn <carlynyepez12@gmail.com>
2022-10-26 11:00:07 -0400

    usuario

--- views/register_user.php
+++ views/register_user.php
@@ -1,4 +1,26 @@
+  <!-- Content Wrapper. Contains page content -->
+  <div class="">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>REGISTRO USURIO</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">formulario</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+
 <form method="post" name="register">
+<section class="content">
+<div class="container-fluid">
 
     <div class="container">
 
@@ -79,4 +101,8 @@
                 <i class="fas fa-upload"></i>
                 <span>GUARDAR</span>
             </button>
-</form>
+</DIV>
+            </section>
+
+        </form>
+</div>
