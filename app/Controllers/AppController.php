025ccda1d449d2295bb0572ed7f899e4d5e04466
yurmi <figueroayurmi@gmail.com>
2022-11-02 21:52:18 -0430

    cambiobitacora

--- app/Controllers/AppController.php
+++ app/Controllers/AppController.php
@@ -213,7 +213,20 @@ class AppController extends BaseController
         View::render('reporte.php');
 
     }
-   
+    public function bitacoraAction()
+    {
+        $bitacora = Bitacora::get()->all();
+    {
+        View::render('bitacora.php',[
+            'bitacora'=>$bitacora
+        ]);
+
+            View::redirect('/user/login');
+        
+} View::render('bitacora.php');
+
+    }
+
     public function estudianteeliminarAction  ()
     {
         $id = $_GET['id'];
--- app/Controllers/Bitacoracontroller.php
+++ app/Controllers/Bitacoracontroller.php
@@ -3,22 +3,23 @@ namespace App\Controllers;
 
 use App\View;
  use App\Models\Bitacora;
- 
-	class Bitacoracontroller extends BaseModel
+
+	class Bitacoracontroller extends BitacoraTrait
 	
-    public function bitacoraAction()
+     
     {
+        public function bitacoraAction()
+     {   
         $bitacora = Bitacora::get()->all();
-    {
+    
         View::render('bitacora.php',[
             'bitacora'=>$bitacora
         ]);
 
-            View::redirect('/user/login');
         
-} 
+ View::render('bitacora.php');
 
     }
 
-		
+	}	
 		
