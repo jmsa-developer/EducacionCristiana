ba6b88501b2a39458fc14a5691fd65b613588240
Carlyn <carlynyepez12@gmail.com>
2022-11-12 12:42:22 -0400

    docente

--- views/docente.php
+++ views/docente.php
@@ -80,7 +80,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
                     </div>
-                    <input type="text" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask name="fecha_nacimiento" id="fecha_nacimiento">
+                    <input type="date" class="form-control" data-inputmask-alias="datetime" data-inputmask-inputformat="dd/mm/yyyy" data-mask name="fecha_nacimiento" id="fecha_nacimiento">
                   
                         </div>
                     </div>                              
